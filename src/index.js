94555d59476dce57088845cda50921f6b7390d93
John McKinnon <johnmckinnon23@yahoo.com>
2022-09-30 14:03:10 -0700

    update

--- src/index.js
+++ src/index.js
@@ -2,6 +2,26 @@ const API = 'http://localhost:3000/Sneakers'
 console.log('hello')
 fetch(API)
   .then(resp => resp.json())
-  .then(json => console.log(json))
+  .then(console.log)
 
-// testing my push
+function renderSneakersToScreen(sneaker) {
+ sneaker.forEach(renderSneakers);
+};
+
+function renderSneakers(sneaker) {
+  const sneakerCollection = document.querySelector('.sneaker-collection');
+  const sneakerName = document.createElement('p');
+  const userName = document.createElement('h3');
+  const imageUrl = document.createElement('img');
+  const sneakerDesc = document.createElement('p');
+  const sneakerLikes = document.createElement('p');
+
+
+  sneakerName.textContent = sneaker.name;
+  userName.textContent = sneaker.username;
+  imageUrl.src = sneaker.image;
+  sneakerDesc.textContent = sneaker.description;
+  sneakerLikes.textContent = sneaker.likes
+
+  sneakerCollection.append(sneakerName, userName, imageUrl, sneakerDesc, sneakerLikes);
+};
