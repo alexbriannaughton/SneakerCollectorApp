72e7ecb9ae4c5f2ab1947052f21cbfc2a805eadb
John McKinnon <johnmckinnon23@yahoo.com>
2022-09-30 13:25:49 -0700

    src updated

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const API = 'http://localhost:3000/Sneakers'
 console.log('hello')
 fetch(API)
-.then(resp => resp.json())
-.then(json=>console.log(json))
+  .then(resp => resp.json())
+  .then(json => console.log(json))
 
 // testing my push
