3fe14310bb1caf73a8ab531c968509d195d4613f
John McKinnon <johnmckinnon23@yahoo.com>
2022-09-30 14:45:38 -0700

    render images done

--- src/index.js
+++ src/index.js
@@ -2,26 +2,27 @@ const API = 'http://localhost:3000/Sneakers'
 console.log('hello')
 fetch(API)
   .then(resp => resp.json())
-  .then(console.log)
+  .then(renderSneakersToScreen)
 
 function renderSneakersToScreen(sneaker) {
  sneaker.forEach(renderSneakers);
 };
 
 function renderSneakers(sneaker) {
-  const sneakerCollection = document.querySelector('.sneaker-collection');
-  const sneakerName = document.createElement('p');
-  const userName = document.createElement('h3');
+  const sneakerCollection = document.querySelector('#sneaker-collection');
+  const sneakerCard = document.createElement('div')
+  const userName = document.createElement('p');
   const imageUrl = document.createElement('img');
   const sneakerDesc = document.createElement('p');
   const sneakerLikes = document.createElement('p');
+  sneakerCard.className = "sneaker-card"
 
-
-  sneakerName.textContent = sneaker.name;
   userName.textContent = sneaker.username;
   imageUrl.src = sneaker.image;
   sneakerDesc.textContent = sneaker.description;
-  sneakerLikes.textContent = sneaker.likes
+  sneakerLikes.textContent = `likes: ${sneaker.likes}`
 
-  sneakerCollection.append(sneakerName, userName, imageUrl, sneakerDesc, sneakerLikes);
+  sneakerCard.append(userName, imageUrl, sneakerDesc, sneakerLikes);
+  sneakerCollection.append(sneakerCard)
 };
+
