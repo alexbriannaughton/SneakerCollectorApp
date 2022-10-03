36083359e4827b82e18d10faa7484c1da21844fd
Alex Brian Naughton <alexbriannaughton@gmail.com>
2022-10-03 10:08:44 -0700

    core deliverables, two more pieces in db.json, shoe images smaller

--- src/index.js
+++ src/index.js
@@ -4,74 +4,82 @@ let currentSneaker;
 
 
 fetch(API)
-.then(resp => resp.json())
-.then(renderSneakersToScreen)
+  .then(resp => resp.json())
+  .then(json => renderSneakersToScreen(json))
 
 function renderSneakersToScreen(sneaker) {
-  sneaker.forEach(renderSneakers);
+  sneaker.forEach(makeSneakerCard)
 };
 
-function renderSneakers(sneaker) {
+
+
+const listenForFormSubmit = () => {
+  const form = document.querySelector('form')
+  form.addEventListener('submit', e => {
+    e.preventDefault();
+    const userName = e.target[0].value;
+    const sneakerImage = e.target[1].value;
+    const description = e.target[2].value
+    const sneakerLikes = 0
+
+    const newSneaker = {
+      name: userName,
+      image: sneakerImage,
+      description: description,
+      likes: sneakerLikes
+    }
+    postSneaker(newSneaker)
+  })
+};
+
+const postSneaker = sneaker => {
+  const config = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'Application/json'
+    },
+    body: JSON.stringify(sneaker)
+  }
+  fetch(API, config)
+    .then(resp => resp.json())
+    .then(json => makeSneakerCard(json))
+}
+
+function makeSneakerCard(sneaker) {
   const sneakerCollection = document.querySelector('#sneaker-collection');
+
   const sneakerCard = document.createElement('div')
   const userName = document.createElement('p');
   const imageUrl = document.createElement('img');
   const sneakerDesc = document.createElement('p');
   const sneakerLikes = document.createElement('p');
   const likeButton = document.createElement('button')
+
   sneakerCard.className = "sneaker-card"
-  
+  imageUrl.className = 'sneaker-pic'
+
   likeButton.textContent = "Like this shoe"
   userName.textContent = sneaker.username;
   imageUrl.src = sneaker.image;
   sneakerDesc.textContent = sneaker.description;
   sneakerLikes.textContent = `likes: ${sneaker.likes}`
-  
-  sneakerCard.append(userName, imageUrl, sneakerDesc,likeButton,sneakerLikes);
-  sneakerCollection.append(sneakerCard)
-  
-  likeButton.addEventListener('click', e=> {
-    currentSneaker = sneaker
-    patchLikes(e, currentSneaker, sneakerLikes)})
-  };
-  
-  const listenForFormSubmit = () => {
-    const form = document.querySelector('form')
-    console.log(form)
-    form.addEventListener('submit', e => {
-      e.preventDefault();
-      const userName = e.target[0].value;
-      const sneakerImage = e.target[1].value;
-      const description = e.target[2].value
-      
-      const newSneaker = {
-        name: userName,
-        image: sneakerImage,
-        description: description 
-      }
-      postSneaker(newSneaker)
-    })
-  };
-  listenForFormSubmit();
-  
-  const postSneaker = sneaker => {
-    const config = {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'Application/json'
-      },
-      body: JSON.stringify(sneaker)
-    }
-    fetch(API, config)
-    .then(resp => resp.json())
-    .then(json=>renderSneakers(json))
-  }
 
-function patchLikes(e, currentSneaker, sneakerLikes){
+  sneakerCard.append(userName, imageUrl, sneakerDesc, likeButton, sneakerLikes);
+  sneakerCollection.append(sneakerCard)
+
+  likeButton.addEventListener('click', e => {
+    currentSneaker = sneaker
+    patchLikes(e, currentSneaker, sneakerLikes)
+  })
+};
+
+function patchLikes(e, currentSneaker, sneakerLikes) {
   console.log(e),
   console.log(currentSneaker)
+
   const likesPlus = currentSneaker.likes + 1
   console.log(likesPlus)
+
   const config = {
     method: 'PATCH',
     headers: {
@@ -82,6 +90,8 @@ function patchLikes(e, currentSneaker, sneakerLikes){
       likes: likesPlus
     })
   }
-  fetch(`http://localhost:3000/toys/${currentSneaker.id}`, config)
-  .then(sneakerLikes.textContent = `${likesPlus} likes`)
+  fetch(`http://localhost:3000/Sneakers/${currentSneaker.id}`, config)
+    .then(sneakerLikes.textContent = `likes: ${likesPlus}`)
 }
+
+listenForFormSubmit();
